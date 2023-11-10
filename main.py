0cb2146356b6d15cc1531b1d8c8cc92b7f416e71
TheOriginalPupp <carlosl01juegos@gmail.com>
2023-11-09 21:19:06 -0600

    Creacion de diccionarios, nombre de las funciones, y guardar los tipos de datos de las variables

--- main.py
+++ main.py
@@ -6,8 +6,27 @@
 # Isaac Mendez Rodriguez / / 6pm
 # Ignacio Ledezma Hidalgo / 402520080 / 8pm
 
+import ast
+
 # Primer paso, leer el archivo
 def leer_archivo(file_name):
     with open(file_name, 'r') as file:
         contenido = file.read()
     return contenido
+
+# Creacion de los diccionarios
+datos_funcion = {} # Diccionario que guarda la informacion de las funciones
+tipos_variables = {} # Diccionario que guarda la informacion de los nombres de los tipos de variables
+
+nombre_funcion = "mi_funcion"
+tipo_retorno = "int"
+
+datos_funcion[nombre_funcion] = tipo_retorno
+
+# Guardar tipos de datos de variables
+tipos_variables['variable1'] = 'int'
+tipos_variables['variable2'] = 'string'
+tipos_variables['variable3'] = 'float'
+tipos_variables['variable4'] = 'void'
+
+codigo_fuente = leer_archivo('Funcion1.txt')
