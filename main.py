5e9d471b580da962d3ed3b868dc27573398aea18
TheOriginalPupp <carlosl01juegos@gmail.com>
2023-11-15 11:14:29 -0600

    Creacion de funciones para mostrar el contenido del archivo y obtener el tipo de funcion. Probadas ambas funciones.

--- main.py
+++ main.py
@@ -6,7 +6,8 @@
 # Isaac Mendez Rodriguez / / 6pm
 # Ignacio Ledezma Hidalgo / 402520080 / 8pm
 
-import ast
+import re
+
 
 # Primer paso, leer el archivo
 def leer_archivo(file_name):
@@ -14,19 +15,40 @@ def leer_archivo(file_name):
         contenido = file.read()
     return contenido
 
-# Creacion de los diccionarios
-datos_funcion = {} # Diccionario que guarda la informacion de las funciones
-tipos_variables = {} # Diccionario que guarda la informacion de los nombres de los tipos de variables
 
-nombre_funcion = "mi_funcion"
-tipo_retorno = "int"
+def mostrar_contenido_archivo(file_name):  # Funcion adicional, para determinar si se esta leyendo correctamente el contenido del archivo
+    try:
+        with open(file_name, 'r') as file:
+            contenido = file.read()
+            print(contenido)  # Imprima lo que tiene el archivo
+    except FileNotFoundError:
+        print(f"El archivo '{file_name}' no fue encontrado.") # Si no existe ningun archivo con ese nombre
+    except Exception as e:
+        print(f"No se pudo leer correctamente el archivo: {e}") # Si ocurre un error no contemplado
 
-datos_funcion[nombre_funcion] = tipo_retorno
 
-# Guardar tipos de datos de variables
-tipos_variables['variable1'] = 'int'
-tipos_variables['variable2'] = 'string'
-tipos_variables['variable3'] = 'float'
-tipos_variables['variable4'] = 'void'
+def obtener_tipo_funcion(file_name):  # Funcion para determinar el tipo de variable de la funcion
+    try:
+        with open(file_name, 'r') as file:
+            contenido = file.read()
 
-codigo_fuente = leer_archivo('Funcion1.txt')
+            match = re.search(r'\b(\w+)\s+\w+\([^)]*\)\s*{', contenido)
+
+            if match:
+                tipo_funcion = match.group(1)
+                return tipo_funcion
+            else:
+                return "No se encontro el tipo de funcion"
+    except FileNotFoundError:
+        return f"El archivo '{file_name}' no existe en este contexto"
+    except Exception as e:
+        return f"No se pudo leer correctamente el archivo: {e}"
+
+
+tipos_variables = {'void', 'int', 'float', 'string'}  # Diccionario que almacena los tipos de variables permitidos
+
+
+nombre_archivo = "Funcion3.txt"
+mostrar_contenido_archivo(nombre_archivo)
+tipo_funcion = obtener_tipo_funcion(nombre_archivo)
+print("\nTipo de variable de la funcion:", tipo_funcion)
